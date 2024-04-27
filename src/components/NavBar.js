708a90c8ac055426fe90c80ed2a74a3d466b8bfe
Kevin Choi <kevinnwjchoi@gmail.com>
2024-04-27 19:29:02 -0400

    made sep ul for navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,10 +6,12 @@ function NavBar(){
         <nav className='nav-bar'>
           <ul>
               <Link to="/">Home </Link>
-              
-              <Link to="/adventurelist">Adventures </Link>
-
-              <Link to="/about">About</Link>
+          </ul>
+          <ul>
+            <Link to="/adventurelist">Adventures </Link>
+          </ul>
+          <ul>
+            <Link to="/about">About</Link>
           </ul>
         </nav>
       );
