951ca0f8a1b1cbbdb02d5553bbb7cf6902abdf06
Kevin Choi <kevinnwjchoi@gmail.com>
2024-04-27 19:08:26 -0400

    filled About with some info about the application

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,17 @@
-
+import React from "react"
 
 function About(){
 
-
+    return(
+        <div>
+        <h2>Welcome to the About page! </h2>
+        <p>This is my Phase 2 project that showcases different vacation spots.</p>
+        <p>Adventure Journal is Single Page Application (SPA) for keeping track of your adventures.
+        Users can add new adventures with details like name, location, image URL, and type. 
+        This application uses a RESTful API to interact with a local db.json file, 
+        which contains an array of adventure objects.</p>
+    </div>
+    )
 
 }
 
