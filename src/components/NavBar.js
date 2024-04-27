0982b180928bddce744a509eb072512dfeddbc5c
Kevin Choi <kevinnwjchoi@gmail.com>
2024-04-27 19:10:08 -0400

    added <Link in the App component. About doesnt work still

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,6 +8,8 @@ function NavBar(){
               <Link to="/">Home </Link>
               
               <Link to="/adventurelist">Adventures </Link>
+
+              <Link to="/about">About</Link>
           </ul>
         </nav>
       );
