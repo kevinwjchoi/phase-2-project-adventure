7c73e2d404184500d54ebd7fe937c96e5fcb467a
Kevin <kevinnwjchoi@gmail.com>
2024-04-30 16:21:39 -0400

    got rid of unused functions/imports. fixed routes in index.js by implementing it using createBrowserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import './styles.css';
-import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom';
+import { Route, Routes} from 'react-router-dom';
 import React , {useEffect, useState} from "react";
 import Home from './Home';
 import NavBar from './NavBar';
@@ -22,18 +22,16 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
     <div className="App">
       <header className="App-header">
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home adventures={adventures} handleNewAdventure={handleNewAdventure} />} />
+        <Route path="*" element={<Home adventures={adventures} handleNewAdventure={handleNewAdventure} />} />
         <Route path="/adventurelist" element={<AdventureList adventures={adventures} />} />
         <Route path="/about" element={<About />} /> 
       </Routes>  
       </header>
     </div>
-    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App';
 import routes from "../src/routes";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter(routes)
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+  );
 
