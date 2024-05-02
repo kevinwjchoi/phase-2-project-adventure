f23af29fa7a22b53fc981ccd9b54a4bb25607deb
Kevin Choi <kevinnwjchoi@gmail.com>
2024-05-02 11:22:09 -0400

    creating the adventure form

--- src/components/AdventureForm.js
+++ src/components/AdventureForm.js
@@ -1,19 +1,30 @@
 import React , {useState} from "react";
 
-function AdventureForm(){
+function AdventureForm({}){
+
+        //all the state/setters for input values 
+        const [inputName, setInputName] = useState("")
+        const [inputImage, setInputImage] = useState("")
+        const [inputLocation, setInputLocation] = useState("")
+        const [inputType, setInputType] = useState("")
 
 
     return (
-
+    <>
+        <header>
+        <h1>Add a new adventure spot!</h1>
+        </header>
+    <div>
         <form onSubmit={handleSubmit}>
-        <h2>Add a new adventure spot!</h2>
         <input type="text" value={inputName} onChange={handleNameChange} placeholder='Enter new adventure name'></input>
         <input type="text" value={inputImage} onChange={handleImageChange}placeholder='Enter image url'></input>
         <input type="text" value={inputLocation} onChange={handleLocationChange} placeholder='Enter image location'></input>
         <input type="text" value={inputType} onChange={handleTypeChange} placeholder='Enter image type'></input>
         <button>Submit</button>
-    </form>
-
+        </form>
+    </div>
+    </>    
+    
 
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,7 @@ import React, {useState} from 'react';
 
 function Home({adventures, handleNewAdventure}) {
 
-    //all the state/setters for input values 
-    const [inputName, setInputName] = useState("")
-    const [inputImage, setInputImage] = useState("")
-    const [inputLocation, setInputLocation] = useState("")
-    const [inputType, setInputType] = useState("")
+
 
 
 
@@ -57,8 +53,6 @@ function Home({adventures, handleNewAdventure}) {
             <p>You can always click 'Home' to return to this home page.</p>
             <p>Click on 'Adventures' to see a whole list of places with images.</p>
             <p>Click on 'About' to learn more about this SPA!</p>
-        
-           
             
         </header>
         <div>
